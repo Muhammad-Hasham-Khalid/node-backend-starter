684a2c6116bed7fc4a84f6225402b2a4622f4fdb
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2022-10-16 20:59:43 +0500

    feat(testing): add tests for signup

--- src/auth/__tests__/auth.controllers.spec.js
+++ src/auth/__tests__/auth.controllers.spec.js
@@ -4,9 +4,7 @@ import request from 'supertest';
 import { config } from '../../config';
 import { User } from '../../resources/user/user.model';
 import { app } from '../../server';
-import { setup } from '../../test-utils';
 import { connect } from '../../utils/db';
-import { signup } from '../controllers';
 
 describe('signin()', () => {
   let user = {
@@ -17,10 +15,7 @@ describe('signin()', () => {
 
   beforeAll(async () => {
     await connect(config.MONGODB_URI);
-
-    const { request, response } = setup();
-    request.body = user;
-    await signup(request, response);
+    await User.create(user);
   });
 
   it('should validate the response.body to have `email` and `password`', async () => {
@@ -50,6 +45,47 @@ describe('signin()', () => {
 
   afterAll(async () => {
     await User.deleteOne({ email: user.email, name: user.name }).exec();
+    await mongoose.disconnect();
+  });
+});
+
+describe('signup()', () => {
+  const testUsers = [];
+  beforeAll(async () => {
+    await connect(config.MONGODB_URI);
+  });
+
+  it('should validate the response.body to have `name`, `email`, `password`', async () => {
+    const response = await request(app).post('/signup').send();
+    expect(response.status).toBe(400);
+  });
+
+  it('should create a new user and return token', async () => {
+    let user = {
+      email: faker.internet.email(),
+      name: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    const response = await request(app).post('/signup').send(user);
+    const expectedResponse = {
+      success: true,
+      token: expect.any(String),
+      message: expect.any(String),
+    };
+
+    expect(response.status).toBe(201);
+    expect(response.body).toMatchObject(expectedResponse);
+
+    testUsers.push(user);
+  });
+
+  afterAll(async () => {
+    const deletions = testUsers.map(user =>
+      User.deleteOne({ name: user.name, email: user.email }).exec()
+    );
+
+    await Promise.all(deletions);
     await mongoose.disconnect();
   });
 });
